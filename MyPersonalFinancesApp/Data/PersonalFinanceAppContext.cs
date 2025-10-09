62c6a0b835f9914f54aba0379407151ab3ccddd1
DESKTOP-7Q6P0P8\nikit <lomal.10715U@gmail.com>
2025-10-09 10:56:39 +0300

    Add project files.

---
+++ MyPersonalFinancesApp/Data/PersonalFinanceAppContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPersonalFinancesApp.Models;
+
+namespace MyPersonalFinancesApp.Data
+{
+    public class PersonalFinanceAppContext : DbContext
+    {
+        public PersonalFinanceAppContext(DbContextOptions<PersonalFinanceAppContext> options):base(options) { }
+
+        DbSet<Expence> Expences { get; set; }
+    }
+}
---
+++ MyPersonalFinancesApp/Models/Expence.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPersonalFinancesApp.Models
+{
+    public class Expence
+    {
+        public int Id { get; set;}
+        public string Description { get; set; } = null!;
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount has to be higher than zero")]
+        public double Amount { get; set; }
+        [Required]
+        public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
